fc5ad91a0355daa51d5f6ebf13f36425b76f6896
Kristian <kristianty80@gmail.com>
2025-07-02 19:53:11 +0200

    Added list of tile sprites

--- src/main/java/org/gameproject/util/TileManager.java
+++ src/main/java/org/gameproject/util/TileManager.java
@@ -1,11 +1,12 @@
 package org.gameproject.util;
 
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import org.gameproject.entities.map.Tile;
 import org.gameproject.view.Game;
 
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -13,21 +14,27 @@ import java.util.Objects;
 public class TileManager {
     private Game game;
     private List<Tile> listOfTiles;
+    private List<BufferedImage> listOfSprites;
 
 
     public TileManager(Game game){
         this.game = game;
         listOfTiles = new ArrayList<>();
-        appendTileImage();
+        listOfSprites = new ArrayList<>();
+        appendTileImages();
     }
 
-    public void appendTileImage(){
-        String tileImagePath = "/tiles/images/";
-        Image grassTileImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream(
-                tileImagePath + "grassland.png")));
-        listOfTiles.add(new Tile(16, 16, grassTileImage));
-        }
+    public void appendTileImages() {
+        try {
+            String tileImagePath = "/tiles/images/";
+            BufferedImage grassTileImage = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(
+                    tileImagePath + "grassland.png")));
+            listOfTiles.add(new Tile(16, 16, grassTileImage));
+            listOfSprites.add(grassTileImage);
+        } catch (Exception e) {
 
+        }
+    }
     public List<Tile> getListOfTiles(){
         return listOfTiles;
     }
