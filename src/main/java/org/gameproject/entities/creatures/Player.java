0f7e415290b333700c9f8dfaccd5f2ea0b197c47
Kristian <kristianty80@gmail.com>
2025-07-02 15:47:38 +0200

    moved position and lastDirection into Creature, load player's sprite images as BufferedImage instead of javafx Image, added exception handling, for failed image loading.

--- src/main/java/org/gameproject/entities/creatures/Player.java
+++ src/main/java/org/gameproject/entities/creatures/Player.java
@@ -1,13 +1,10 @@
-package org.gameproject.entities;
+package org.gameproject.entities.creatures;
 
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
 import org.gameproject.util.KeyHandler;
 import org.gameproject.view.Game;
 
-import javax.imageio.IIOException;
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Objects;
@@ -20,39 +17,75 @@ public class Player extends Creature {
 
     Game gameWindow;
     KeyHandler keyHandler = KeyHandler.get();
-    //Default position
-    private int playerXPos = 100; // Default player X position
-    private  int playerYPos = 100; // Default player Y position
-    private final int playerSpeed = 4; // Default player speed
-    private String lastDirection = "down"; // Last direction the player moved
+    private final int playerSpeed = 3; // Default player speed
+
+
 
 
     public Player(Game gameWindow){
         super("Stig", 100, 12, 5, 1);
         this.gameWindow = gameWindow;
         loadPlayerImage(); // Load player images
+        xPos = 100;
+        yPos = 100;
     }
 
     /**
      * Loads the images related to character movement.
      */
     public void loadPlayerImage() {
-        String path = "/player/images/";
-            up1 = new Image(Objects.requireNonNull(getClass().getResourceAsStream(path + "playerUp1.png")));
-            up2 = new Image(Objects.requireNonNull(getClass().getResourceAsStream(path + "playerUp2.png")));
-            down1 = new Image(Objects.requireNonNull(getClass().getResourceAsStream(path + "playerDown1.png")));
-            down2 = new Image(Objects.requireNonNull(getClass().getResourceAsStream(path + "playerDown2.png")));
-            left1 = new Image(Objects.requireNonNull(getClass().getResourceAsStream(path + "playerLeft1.png")));
-            left2 = new Image(Objects.requireNonNull(getClass().getResourceAsStream(path + "playerLeft2.png")));
-            right1 = new Image(Objects.requireNonNull(getClass().getResourceAsStream(path + "playerRight1.png")));
-            right2 = new Image(Objects.requireNonNull(getClass().getResourceAsStream(path + "playerRight2.png")));
-            idleDown = new Image(Objects.requireNonNull(getClass().getResourceAsStream(path + "idleDown.png")));
-            idleLeft= new Image(Objects.requireNonNull(getClass().getResourceAsStream(path + "idleLeft.png")));
-            idleRight = new Image(Objects.requireNonNull(getClass().getResourceAsStream(path + "idleRight.png")));
-            idleUp = new Image(Objects.requireNonNull(getClass().getResourceAsStream(path + "idleUp.png")));
+        boolean methodPassed = false;
+        int numberOfAttempts = 0;
+        int maxAttempts = 3;
+        while (!methodPassed && numberOfAttempts < maxAttempts)
+            try {
+                String path = "/player/images/";
+                up1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(path + "playerUp1.png")));
+                up2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(path + "playerUp2.png")));
+                down1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(path + "playerDown1.png")));
+                down2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(path + "playerDown2.png")));
+                left1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(path + "playerLeft1.png")));
+                left2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(path + "playerLeft2.png")));
+                right1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(path + "playerRight1.png")));
+                right2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(path + "playerRight2.png")));
+                idleDown = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(path + "idleDown.png")));
+                idleLeft = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(path + "idleLeft.png")));
+                idleRight = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(path + "idleRight.png")));
+                idleUp = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(path + "idleUp.png")));
+                methodPassed = true;
 
+            } catch (Exception e) {
+            numberOfAttempts++;
+            if (numberOfAttempts == maxAttempts){
+                System.err.println("Reading images failed after " + numberOfAttempts + " attempts");
+                System.err.println("Likely cause: images are " + e.getMessage());
+                handleImageLoadError(e);
+            }
+            }
     }
 
+    private void handleImageLoadError(Exception e){
+        System.err.println("Error details: " + e.getClass().getName());
+
+        loadPlayerImageFallback();
+
+        gameWindow.writeErrorToFile("Failed to load player assets", e);
+
+    }
+    private void loadPlayerImageFallback(){
+        BufferedImage fallback = new BufferedImage(32, 32, BufferedImage.TYPE_INT_ARGB);
+        java.awt.Graphics2D graphics = fallback.createGraphics();
+        graphics.setColor(Color.MAGENTA);
+        graphics.fillRect(0,0,gameWindow.getTileSize(), gameWindow.getTileSize());
+
+        up1 = up2 = down1 = down2 = left1 = left2 = right1 = right2 =
+                idleDown = idleLeft = idleRight = idleUp = fallback;
+        graphics.dispose();
+
+        System.err.println("Using fallback images due to load error in loadPlayerImage()");
+
+
+    }
     public void attack(){
 
     }
@@ -61,21 +94,26 @@ public class Player extends Creature {
      * Updates the game state based on user input.
      * This method is called in a loop to continuously update the game logic.
      */
+    @Override
     public void update(){
         if (keyHandler.isUpPressed()) {
-            playerYPos -= playerSpeed; // Move player up
+            isMoving = true;
+            yPos -= playerSpeed; // Move player up
             lastDirection = "up";
         }
         if (keyHandler.isDownPressed()) {
-            playerYPos += playerSpeed; // Move player down
+            isMoving = true;
+            yPos += playerSpeed; // Move player down
             lastDirection = "down";
         }
         if (keyHandler.isLeftPressed()) {
-            playerXPos -= playerSpeed; // Move player left
+            isMoving = true;
+            xPos -= playerSpeed; // Move player left
             lastDirection = "left";
         }
         if (keyHandler.isRightPressed()) {
-            playerXPos += playerSpeed; // Move player right
+            isMoving = true;
+            xPos += playerSpeed; // Move player right
             lastDirection = "right";
         }
         spriteCounter++;
@@ -89,58 +127,21 @@ public class Player extends Creature {
             spriteCounter = 0; // Reset counter
         }
 
-    }
-
-    public void draw(GraphicsContext gc) {
-        gc.setFill(Color.DARKBLUE);
-
-        gc.fillRect(0,0, gameWindow.getScreenWidth(), gameWindow.getScreenHeight());
-        gc.setImageSmoothing(false); //Looks like shit with smoothing on.
-
-        if (keyHandler.isLeftPressed()){
-            if (spriteNum == 1){
-                gc.drawImage(left1, playerXPos, playerYPos, gameWindow.getTileSize(), gameWindow.getTileSize());
-            } else if (spriteNum == 2) {
-                gc.drawImage(left2, playerXPos, playerYPos, gameWindow.getTileSize(), gameWindow.getTileSize());
-            }
-
-        } else if (keyHandler.isDownPressed()){
-            if (spriteNum == 1){
-                gc.drawImage(down1, playerXPos, playerYPos, gameWindow.getTileSize(), gameWindow.getTileSize());
-            } else if (spriteNum == 2) {
-                gc.drawImage(down2, playerXPos, playerYPos, gameWindow.getTileSize(), gameWindow.getTileSize());
-            }
-        } else if (keyHandler.isRightPressed()){
-            if (spriteNum == 1){
-                gc.drawImage(right1, playerXPos, playerYPos, gameWindow.getTileSize(), gameWindow.getTileSize());
-            } else if (spriteNum == 2) {
-                gc.drawImage(right2, playerXPos, playerYPos, gameWindow.getTileSize(), gameWindow.getTileSize());
-            }
-
-        } else if (keyHandler.isUpPressed()){
-            if (spriteNum == 1){
-                gc.drawImage(up1, playerXPos, playerYPos, gameWindow.getTileSize(), gameWindow.getTileSize());
-            } else if (spriteNum == 2) {
-                gc.drawImage(up2, playerXPos, playerYPos, gameWindow.getTileSize(), gameWindow.getTileSize());
-            }
+        if (!keyHandler.isUpPressed() && !keyHandler.isDownPressed()
+                && !keyHandler.isLeftPressed() && !keyHandler.isRightPressed()) {
+            isMoving = false;
         }
-        else {
-            switch (lastDirection){
-            case "up" -> gc.drawImage(idleUp, playerXPos, playerYPos, gameWindow.getTileSize(), gameWindow.getTileSize());
-            case "down" -> gc.drawImage(idleDown, playerXPos, playerYPos, gameWindow.getTileSize(), gameWindow.getTileSize());
-            case "left" -> gc.drawImage(idleLeft, playerXPos, playerYPos, gameWindow.getTileSize(), gameWindow.getTileSize());
-            case "right" -> gc.drawImage(idleRight, playerXPos, playerYPos, gameWindow.getTileSize(), gameWindow.getTileSize());
-            }
-        }
+
     }
-
-
-
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
