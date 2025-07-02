9375f674608ac1466262afa85c1c896ce4f84546
Kristian <kristianty80@gmail.com>
2025-07-02 15:53:26 +0200

    Holds images of tiles

--- src/main/java/org/gameproject/util/TileManager.java
+++ src/main/java/org/gameproject/util/TileManager.java
@@ -1,4 +1,34 @@
 package org.gameproject.util;
 
-public class TileManage {
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import org.gameproject.entities.map.Tile;
+import org.gameproject.view.Game;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class TileManager {
+    private Game game;
+    private List<Tile> listOfTiles;
+
+
+    public TileManager(Game game){
+        this.game = game;
+        listOfTiles = new ArrayList<>();
+        appendTileImage();
+    }
+
+    public void appendTileImage(){
+        String tileImagePath = "/tiles/images/";
+        Image grassTileImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream(
+                tileImagePath + "grassland.png")));
+        listOfTiles.add(new Tile(16, 16, grassTileImage));
+        }
+
+    public List<Tile> getListOfTiles(){
+        return listOfTiles;
+    }
 }
