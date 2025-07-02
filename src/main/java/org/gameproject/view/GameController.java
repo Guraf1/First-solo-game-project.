777031777db34b69e5c39101d6c74956430855f6
Kristian <kristianty80@gmail.com>
2025-07-02 15:49:34 +0200

    Delegates rendering tasks from Game, to Renderer

--- src/main/java/org/gameproject/view/GameController.java
+++ src/main/java/org/gameproject/view/GameController.java
@@ -1,4 +1,44 @@
 package org.gameproject.view;
 
+import javafx.scene.canvas.GraphicsContext;
+import org.gameproject.entities.creatures.Creature;
+import org.gameproject.util.TileManager;
+
 public class GameController {
+
+    private Game game;
+    private Renderer renderer;
+    private TileManager tileManager;
+
+    public GameController(Game game){
+        this.game = game;
+        this.tileManager = new TileManager(game);
+        this.renderer = new Renderer(this.game,
+                null,
+                null,
+                this.tileManager);
+    }
+
+    public void drawCreature(Creature creature){
+        GraphicsContext gc = game.getCanvas().getGraphicsContext2D();
+        this.renderer.setGraphicsContext(gc);
+        this.renderer.setCreature(creature);
+        renderer.drawCreature();
+
+    }
+
+    public void clearOldFrame(){
+        GraphicsContext gc = game.getCanvas().getGraphicsContext2D();
+        this.renderer.setGraphicsContext(gc);
+//        Renderer renderer = new Renderer(this.game, gc, null);
+        renderer.clear();
+    }
+
+    public void drawTiles(){
+        GraphicsContext gc = game.getCanvas().getGraphicsContext2D();
+        this.renderer.setGraphicsContext(gc);
+        this.renderer.drawTiles();
+    }
+
+
 }
