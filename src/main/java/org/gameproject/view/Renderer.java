0e0d74cfe8c08389158be2c9f06972cc47a6dd7a
Kristian <kristianty80@gmail.com>
2025-07-02 15:51:36 +0200

    Changed class to general renderer, now handles all rendering activity

--- src/main/java/org/gameproject/view/Renderer.java
+++ src/main/java/org/gameproject/view/Renderer.java
@@ -1,25 +1,31 @@
 package org.gameproject.view;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.embed.swing.SwingFXUtils;
 import org.gameproject.entities.creatures.Creature;
-import org.gameproject.entities.creatures.Player;
 import org.gameproject.util.KeyHandler;
+import org.gameproject.util.TileManager;
 
-public class CreatureRenderer {
+public class Renderer {
 
     private Game game;
     private Creature creature;
     private GraphicsContext gc;
-    private final KeyHandler keyHandler = KeyHandler.get();
+    private TileManager tileManager;
 
-    public CreatureRenderer(Game game, GraphicsContext gc, Creature creature){
+    public Renderer(Game game, GraphicsContext gc, Creature creature, TileManager tileManager){
         this.game = game;
         this.creature = creature;
         this.gc = gc;
+        this.tileManager = tileManager;
     }
 
-    public void drawCreature(){
-        if (creature == null) {return;} //Exit method if there is no creature.
+
+
+    public void drawCreature() {
+        if (creature == null) {
+            return;
+        } //Exit method if there is no creature.
 
         int xPos = creature.getxPos();
         int yPos = creature.getYPos();
@@ -27,26 +33,66 @@ public class CreatureRenderer {
 
         String direction = creature.getLastDirection();
         int spriteNumber = creature.getSpriteNum();
-        boolean isMoving = creature.isMoving();
 
         String spriteKey;
-        if (isMoving){
+        if (creature.isMoving()) {
             spriteKey = direction + spriteNumber; //e.g. left1 (to get the correct sprite)
         } else {
-            spriteKey = "idle" + capitalize(direction);
+            switch (direction) {
+                case "up":
+                    spriteKey = "idleUp";
+                    break;
+                case "left":
+                    spriteKey = "idleLeft";
+                    break;
+                case "right":
+                    spriteKey = "idleRight";
+                    break;
+                case "down":
+                    spriteKey = "idleDown";
+                default:
+                    spriteKey = "idle" + capitalize(direction);
+            }
         }
-
-        gc.drawImage(creature.getSprite(spriteKey),
+        gc.drawImage(
+                SwingFXUtils.toFXImage(creature.getSprite(spriteKey), null),
                 xPos,
                 yPos,
                 game.getTileSize(),
-                game.getTileSize()
-        );
+                game.getTileSize());
     }
-
     private String capitalize(String string){
         if (string == null || string.isEmpty()) return string;
         return string.substring(0, 1).toUpperCase() + string.substring(1);
+    }
 
+    public void drawTiles() {
+        int column = 0;
+        int row = 0;
+        int x = 0;
+        int y = 0;
+        while (column < game.getMaxScreenColumn() && row < game.getMaxScreenRow()) {
+            gc.drawImage(tileManager.getListOfTiles().getFirst().getTileImage(), x, y, game.getTileSize(), game.getTileSize());
+            column++;
+            x += game.getTileSize();
+
+            if (column == game.getMaxScreenColumn()) {
+                column = 0;
+                x = 0;
+                row++;
+                y += game.getTileSize();
+            }
+        }
+    }
+    public void clear(){
+        gc.clearRect(0,0, game.getScreenWidth(), game.getScreenHeight());
+    }
+
+    public void setGraphicsContext(GraphicsContext gc){
+        this.gc = gc;
+    }
+
+    public void setCreature(Creature creature){
+        this.creature = creature;
     }
 }
