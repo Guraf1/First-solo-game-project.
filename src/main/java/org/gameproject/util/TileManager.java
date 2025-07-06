2ca5ee3dc621d0020a508d62ea5c31ff391b1532
Kristian <kristianty80@gmail.com>
2025-07-06 14:49:02 +0200

    Updated with tile builder syntax

--- src/main/java/org/gameproject/util/TileManager.java
+++ src/main/java/org/gameproject/util/TileManager.java
@@ -6,6 +6,7 @@ import org.gameproject.view.Game;
 
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,16 +26,70 @@ public class TileManager {
     }
 
     public void appendTileImages() {
-        try {
-            String tileImagePath = "/tiles/images/";
-            BufferedImage grassTileImage = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(
-                    tileImagePath + "grassland.png")));
-            listOfTiles.add(new Tile(16, 16, grassTileImage));
-            listOfSprites.add(grassTileImage);
-        } catch (Exception e) {
+        boolean methodSuccessful = false;
+        int numberOfAttempts = 0;
 
+        while (!methodSuccessful && numberOfAttempts < 3) {
+            try {
+                String tileImagePath = "/tiles/images/";
+                BufferedImage grassTileImage = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(
+                        tileImagePath + "grassland.png")));
+                BufferedImage treeFlavour = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(tileImagePath + "/spice/tree.png")));
+                listOfTiles.add(new Tile.TileBuilder(16, 16, grassTileImage).build());
+                listOfSprites.add(grassTileImage);
+
+                BufferedImage mossyRoadImage = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(
+                        tileImagePath + "mossyRoad.png")));
+                listOfTiles.add(new Tile.TileBuilder(16,16,mossyRoadImage).build());
+                listOfSprites.add(mossyRoadImage);
+
+                BufferedImage stoneRoadImage = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(
+                        tileImagePath + "stoneRoad.png")));
+                listOfTiles.add(new Tile.TileBuilder(16,16, stoneRoadImage).build());
+                listOfSprites.add(stoneRoadImage);
+
+                BufferedImage oceanImage = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(
+                        tileImagePath + "ocean.png")));
+                listOfTiles.add(new Tile.TileBuilder(16,16,oceanImage).build());
+                listOfSprites.add(oceanImage);
+
+
+                methodSuccessful = true; //Method succeeds at the end of the loop if no exception is thrown.
+
+            } catch (Exception e) {
+                numberOfAttempts++;
+                if (numberOfAttempts == 3) {
+                    System.err.println("Reading tile images failed after " + numberOfAttempts + " attempts");
+                    e.printStackTrace();
+                    loadTileFallbackImages();
+                }
+            }
         }
     }
+
+    /**
+     * Handles the error when loading tile images fails.
+     * This method is called when the tile images cannot be loaded after multiple attempts.
+     */
+    private void loadTileFallbackImages(){
+        System.err.println("Loading fallback images for tiles.");
+        listOfTiles.clear();
+        BufferedImage fallbackImagePink = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D pinkTileGraphics = fallbackImagePink.createGraphics();
+        pinkTileGraphics.setColor(Color.PINK);
+        pinkTileGraphics.fillRect(0, 0, 16, 16);
+        listOfTiles.add(new Tile.TileBuilder(16,16,fallbackImagePink).build());
+        pinkTileGraphics.dispose();
+
+        BufferedImage fallbackImageBlack = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D blackTileGraphics = fallbackImageBlack.createGraphics();
+        blackTileGraphics.setColor(Color.BLACK);
+        blackTileGraphics.fillRect(0, 0, 16, 16);
+        listOfTiles.add(new Tile.TileBuilder(16,16,fallbackImageBlack).build());
+        blackTileGraphics.dispose();
+        System.err.println("Fallback image loaded successfully.");
+    }
+
     public List<Tile> getListOfTiles(){
         return listOfTiles;
     }
