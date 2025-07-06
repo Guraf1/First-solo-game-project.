ac6275ffee4917e12faf69ae4d9465b1bc4a3485
Kristian <kristianty80@gmail.com>
2025-07-06 14:47:54 +0200

    Refactored tile to accomodate builder pattern

--- src/main/java/org/gameproject/entities/map/Tile.java
+++ src/main/java/org/gameproject/entities/map/Tile.java
@@ -12,52 +12,43 @@ public class Tile {
     protected boolean collision = false;
 
     protected BufferedImage tileImage;
-    protected BufferedImage tileImageFlavour;
 //    protected Image tileImage; //Base background image
 //    protected Image tileImageFlavour; //Additional spice on top of the base image
 
-    /**
-     * Constructor for setting all the fields.
-     * @param tileWidth The width of one tile in pixels.
-     * @param tileHeight The height of one tile in pixels.
-     * @param interactable Defines whether a tile is interactable or not.
-     * @param collision Defines if entities can move on top of this tile or not.
-     * @param tileImage The image a tile should represent.
-     */
-    public Tile(int tileWidth, int tileHeight, boolean interactable, boolean collision, BufferedImage tileImage,
-                BufferedImage tileImageFlavour){
-    this.tileWidth = tileWidth;
-    this.tileHeight = tileHeight;
-    this.interactable = interactable;
-    this.collision = collision;
-    this.tileImage = tileImage;
-    this.tileImageFlavour = tileImageFlavour;
+    private Tile(TileBuilder blueprint){
+        this.tileWidth = blueprint.tileWidth;
+        this.tileHeight = blueprint.tileHeight;
+        this.tileImage = blueprint.tileImage;
+        this.interactable = blueprint.interactable;
+        this.collision = blueprint.collision;
     }
 
-    /**
-     * Constructor for setting fields without default values.
-     * @param tileWidth The width of the tile in pixels.
-     * @param tileHeight The height of the tile in pixels.
-     * @param tileImage The image a tile should represent.
-     * @param tileImageFlavour An image to put on top of tileImage.
-     */
-    public Tile(int tileWidth, int tileHeight, BufferedImage tileImage, BufferedImage tileImageFlavour){
-        this.tileWidth = tileWidth;
-        this.tileHeight = tileHeight;
-        this.tileImage = tileImage;
-        this.tileImageFlavour = tileImageFlavour;
-    }
+    public static class TileBuilder{
+        private final int tileWidth;
+        private final int tileHeight;
+        private final BufferedImage tileImage;
+        private boolean collision = false;
+        private boolean interactable = false;
 
-    /**
-     * Barebone version of tile, only the necessary parts.
-     * @param tileWidth The width of the tile in pixels.
-     * @param tileHeight The height of the tile in pixels.
-     * @param tileImage The image of the tile.
-     */
-    public Tile(int tileWidth, int tileHeight, BufferedImage tileImage){
-        this.tileWidth = tileWidth;
-        this.tileHeight = tileHeight;
-        this.tileImage = tileImage;
+        public TileBuilder(int tileWidth, int tileHeight, BufferedImage tileImage){
+            this.tileWidth = tileWidth;
+            this.tileHeight = tileHeight;
+            this.tileImage = tileImage;
+        }
+
+        public TileBuilder interactable(boolean interactable){
+            this.interactable = interactable;
+            return this;
+        }
+
+        public TileBuilder collision(boolean collision){
+            this.collision = collision;
+            return this;
+        }
+
+        public Tile build(){
+            return new Tile(this);
+        }
     }
 
     public BufferedImage getTileImage(){
