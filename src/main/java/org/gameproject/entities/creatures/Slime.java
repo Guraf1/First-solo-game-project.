a9d34a8ce51e52eeae02a82fa4a8b5aecf7c1c4c
kristian <kristianty80@gmail.com>
2025-08-14 22:43:23 +0200

    Test methods.

--- src/main/java/org/gameproject/entities/creatures/Slime.java
+++ src/main/java/org/gameproject/entities/creatures/Slime.java
@@ -1,15 +1,16 @@
 package org.gameproject.entities.creatures;
 
 import org.gameproject.events.OnDeathEvent;
-import org.gameproject.observer.Observer;
 
-public class Slime extends Creature implements Observer, Predator, Updateable{
+
+public class Slime extends Creature implements Predator, Updateable{
 
     private int moneyReward = 10;
     private int experienceReward = 5;
 
     public Slime(String name, double health, double attackPower, double defence, int level) {
         super(name, health, attackPower, defence, level);
+        this.creatureType = "Slime";
     }
 
     @Override
