a4376aa62fb89519cf904d621463b47ead648560
kristian <kristianty80@gmail.com>
2025-08-14 22:37:03 +0200

    Added empty test methods.

--- src/main/java/org/gameproject/entities/creatures/Pig.java
+++ src/main/java/org/gameproject/entities/creatures/Pig.java
@@ -4,12 +4,26 @@ public class Pig extends Creature {
 
     public Pig(String name, double health, double attackPower, double defence, int level) {
         super(name, health, attackPower, defence, level);
+        this.creatureType = "Pig";
     }
 
     public void update(){
 
     }
 
+    @Override
+    public void notify(Object event) {
+        // Handle notification events specific to Pig
+        System.out.println("Pig received event: " + event);
+    }
+
+    @Override
+    public void onDeath(){
+        this.isAlive = false;
+        // Handle death logic for Pig
+        System.out.println("Mr. Pig has died.");
+        // You can trigger an event here if needed
+    }
 
 
 
